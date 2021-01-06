f26e9ddd0a4556eb55ff4a5a0b63a3f3743d0dd6
Simmigon <simmigonflagg@gmail.com>
2021-01-06 15:39:19 -0500

    first Commit

---
+++ config.js
@@ -0,0 +1,15 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDlmK4-vGPsEc3hQfn0r4q1S8Vhe-tP_YM",
+    authDomain: "kaipherfirebaseapp.firebaseapp.com",
+    databaseURL: "https://kaipherfirebaseapp.firebaseio.com",
+    projectId: "kaipherfirebaseapp",
+    storageBucket: "kaipherfirebaseapp.appspot.com",
+    messagingSenderId: "1011528898696",
+    appId: "1:1011528898696:web:1bcc360a091f89932ca73d",
+    measurementId: "G-MHCXTSJ89E"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+  var db = firebase.firestore();
+  
